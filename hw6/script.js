14be75df29b48ac01d5438ab67277e47d78a32e6
Sp1riT42 <pohunov@gmail.com>
2021-03-15 18:39:56 +0300

    Added changes to files

--- hw6/script.js
+++ hw6/script.js
@@ -20,26 +20,22 @@ const gallery = {
     galleryBox: null,
     galleryOverlay: null,
     galleryCloseBtn: null,
-    init() {
-        product.catalog.addEventListener('click', this.render.bind(this))
-        cart.cartBlock.addEventListener('click', this.render.bind(this))
-    },
     render() {
+
         if (event.target.tagName === 'IMG') {
             if (this.srcObj === null) {
-                console.log(event.target.getAttribute('src'))
-                console.log(this.galleryArr.find(item => item.minImg === event.target.getAttribute('src')))
                 this.srcObj = Object.assign({}, this.galleryArr.find(item => item.minImg === event.target.getAttribute('src')))
                 if (this.srcObj !== undefined) {
-                    document.body.insertAdjacentHTML("beforeend", `<div class="gallery-overlay"></div>`)
-                    document.body.insertAdjacentHTML("beforeend", `<div class="gallery-close">X</div>`)
-                    document.body.insertAdjacentHTML("beforeend", `<div class="gallery-box"><img class="gallery-photo" src="${this.srcObj.fullImg}"></div>`)
+                    document.body.insertAdjacentHTML("beforeend", `<div class="gallery-overlay"></div>
+                    <div class="gallery-close">X</div>
+                    <div class="gallery-box">
+                        <img class="gallery-photo" src="${this.srcObj.fullImg}">
+                    </div>`)
                     this.galleryOverlay = document.querySelector('.gallery-overlay')
                     this.galleryCloseBtn = document.querySelector('.gallery-close')
                     this.galleryPhoto = document.querySelector('.gallery-photo')
                     this.galleryBox = document.querySelector('.gallery-box')
-                    this.galleryBox.insertAdjacentHTML("beforeend", `<div class="gallery-left"><</div>`)
-                    this.galleryBox.insertAdjacentHTML("beforeend", `<div class="gallery-right">></div>`)
+                    this.galleryBox.insertAdjacentHTML("beforeend", `<div class="gallery-left"><</div><div class="gallery-right">></div>`)
                     this.galleryBox.addEventListener('click', this.slide.bind(this))
                     this.galleryCloseBtn.addEventListener('click', this.galleryClose.bind(this))
                 }
@@ -51,8 +47,6 @@ const gallery = {
                 this.galleryCloseBtn.classList.toggle('hide')
 
             }
-
-
         }
 
     },
@@ -136,6 +130,8 @@ let cart = {
     ],
     cartBlock: null,
     cartButton: null,
+    goods: null,
+    gallery,
     getCount() {
         return this.cartArr.length
     },
@@ -155,20 +151,21 @@ let cart = {
         } else {
             this.cartBlock.insertAdjacentHTML("beforeend", `<div class="goods-box"></div>`)
             let cartBlockBox = document.querySelector('.goods-box')
-            for (let key in this.cartArr) {
-                cartBlockBox.insertAdjacentHTML("beforeend", `<div class="goods"></div>`)
-                let goods = document.querySelectorAll('.goods')
-                goods[key].insertAdjacentHTML("beforeend", `<div class="img__box"><img class="photo" src="${this.cartArr[key].src}"></div>`)
-                goods[key].insertAdjacentHTML("beforeend", `<p>Название товара: ${this.cartArr[key].name}</p>`)
-                goods[key].insertAdjacentHTML("beforeend", `<p>Цена за единицу: ${this.cartArr[key].price}</p>`)
-                goods[key].insertAdjacentHTML("beforeend", `<p>Количество: ${this.cartArr[key].count}</p>`)
-
+            for (let i = 0; i < this.cartArr.length; i++) {
+                cartBlockBox.insertAdjacentHTML("beforeend", `<div class="goods">
+                <div class="img__box">
+                <img class="photo" src="${this.cartArr[i].src}">
+                <p>Название товара: ${this.cartArr[i].name}</p>
+                <p>Цена за единицу: ${this.cartArr[i].price}</p>
+                <p>Количество: ${this.cartArr[i].count}</p>
+                </div>
+                </div>`)
             }
             this.cartBlock.insertAdjacentHTML("beforeend", '<p class="cart__text">В корзине позиций товаров: ' + this.getCount() + 'шт. на сумму ' + this.getSum() + ' рублей</p>')
         }
+        this.cartBlock.addEventListener('click', this.gallery.render.bind(this.gallery))
     },
     clearCart() {
-
         this.cartArr = []
         this.render()
     }
@@ -207,38 +204,32 @@ let product = {
         }
     ],
     catalog: document.querySelector('#catalog'),
+    cart,
+    gallery,
     render() {
-        let product
-        this.catalog.style.cssText = 'display: flex; border: 1px solid black; padding: 24px; margin: 20px;'
         for (let i = 0; i < this.catalogArr.length; i++) {
-            this.catalog.insertAdjacentHTML("beforeend", `<div class="product"></div>`)
-            product = document.querySelectorAll('.product')
-            product[i].insertAdjacentHTML("beforeend", `<a class="button__buy" id="${this.catalogArr[i].id}" href="#">Купить</a>`)
-            let buttonBuy = document.querySelectorAll('.button__buy')
-            buttonBuy[i].addEventListener('click', this.addCart.bind(this))
-            for (let j = 0; j < Object.keys(this.catalogArr[i]).length; j++) {
-                if (Object.keys(this.catalogArr[i])[j] === 'id') continue
-                if (Object.keys(this.catalogArr[i])[j] === 'src') {
-                    product[i].insertAdjacentHTML("afterbegin", `<div class="img__box"><img class="photo" src="${this.catalogArr[i].src}"></div>`)
-                    continue
-                }
-                product[i].insertAdjacentHTML("beforeend", `<p>${Object.keys(this.catalogArr[i])[j]}: ${Object.values(this.catalogArr[i])[j]}</p>`)
-            }
+            this.catalog.insertAdjacentHTML("beforeend", `<div class="product">
+            <div class="img__box"><img class="photo" src="${this.catalogArr[i].src}"></div>
+                <a class="button__buy" id="${this.catalogArr[i].id}" href="#">Купить</a>
+                <p>Название товара: ${this.catalogArr[i].name}</p>
+                <p>Цена за единицу: ${this.catalogArr[i].price}</p>
+                <p>Количество: ${this.catalogArr[i].count}</p>
+            </div>`)
         }
-        for (let val of product) {
-            val.style.cssText = 'border: 1px solid black; padding: 8px; margin: 0 auto 10px;'
-        }
-        gallery.init()
+        catalog.addEventListener('click', this.addCart.bind(this))
+        this.catalog.addEventListener('click', this.gallery.render.bind(this.gallery))
     },
     addCart() {
-        let targetObj = this.catalogArr.find(item => item.id === Number(event.target.id))
-        if (cart.cartArr.find(item => item.id === Number(event.target.id))) {
-            cart.cartArr.find(item => item.id === targetObj.id).count += targetObj.count
-            cart.render()
-            return
+        if (event.target.className === "button__buy") {
+            let targetObj = this.catalogArr.find(item => item.id === Number(event.target.id))
+            if (cart.cartArr.find(item => item.id === Number(event.target.id))) {
+                cart.cartArr.find(item => item.id === targetObj.id).count += targetObj.count
+                cart.render()
+                return
+            }
+            this.cart.cartArr.push(Object.assign({}, this.catalogArr.find(item => item.id === Number(event.target.id))))
+            this.cart.render()
         }
-        cart.cartArr.push(Object.assign({}, this.catalogArr.find(item => item.id === Number(event.target.id))))
-        cart.render()
     }
 }
 product.render()
